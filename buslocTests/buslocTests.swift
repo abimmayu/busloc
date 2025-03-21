340df3345e1e4fe2dfbfbcebad7c3f6c1b8f991f
abimmayu <sakilavino@gmail.com>
2025-03-21 15:48:09 +0700

    Initial Commit

---
+++ buslocTests/buslocTests.swift
@@ -0,0 +1,17 @@
+//
+//  buslocTests.swift
+//  buslocTests
+//
+//  Created by Abim on 21/03/25.
+//
+
+import Testing
+@testable import busloc
+
+struct buslocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
